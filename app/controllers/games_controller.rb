6e86d592fa974a7e9e32ce517095f1cec55666c4
Sacha Monneger <smonneger@gmail.com>
2022-10-01 14:48:36 +0200

    add route to display search page

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,4 +2,8 @@ class GamesController < ApplicationController
   def index
     render template: "static/index"
   end
+
+  def search
+    render template: "static/search"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "games#index"
 
+  get  "search", to: "games#search"
+
   namespace "api" do
     resources "games", only: %i[index create update destroy]
   end

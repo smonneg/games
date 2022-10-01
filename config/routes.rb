ba273fbeacc697e2c727f3cb8bda6468998e6885
Sacha Monneger <smonneger@gmail.com>
2022-10-01 15:39:54 +0200

    Rename search.html to index.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   root "games#index"
 
-  get  "search", to: "games#search"
-
   namespace "api" do
     resources "games", only: %i[index create update destroy] do
       collection do
